5ede8a34e5452ad7c83de20b766588eded2eeca3
Jonathan Cardoso <jonathancs2332@gmail.com>
2025-07-30 14:05:54 -0300

    added external category di

--- lib/core/external/di/external_dependencies.dart
+++ lib/core/external/di/external_dependencies.dart
@@ -1,12 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:so_bicos/core/data/datasources/auth/auth_datasource.dart';
+import 'package:so_bicos/core/data/datasources/job/job_category_datasource.dart';
 import 'package:so_bicos/core/external/datasources/auth/firebase_datasource_impl.dart';
+import 'package:so_bicos/core/external/datasources/job/category_firestore_datasource_impl.dart';
 
 List<SingleChildWidget> get externalDependencies {
   return [
     Provider(create: (context) => FirebaseAuth.instance),
+    Provider(create: (context) => FirebaseFirestore.instance),
     Provider(create: (context) => FirebaseDataSourceImpl(auth: context.read()) as AuthDataSource),
+    Provider(create: (context) => CategoryFirestoreDataSourceImpl(db: context.read()) as JobCategoryDataSource),
   ];
 }
