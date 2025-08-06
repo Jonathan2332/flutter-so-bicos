a3b69ad29e14639222b94cd1f80e1f29925fc523
Jonathan Cardoso <jonathancs2332@gmail.com>
2025-08-06 12:27:52 -0300

    upated domain job model

--- lib/core/domain/models/job/job.dart
+++ lib/core/domain/models/job/job.dart
@@ -1,12 +1,16 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:so_bicos/core/domain/models/job/job_category.dart';
+
 class Job {
   String title;
   String description;
+  JobCategory category;
   String author;
-  int date;
+  DateTime date;
   Job({
     required this.title,
     required this.description,
+    required this.category,
     required this.author,
     required this.date,
   });
