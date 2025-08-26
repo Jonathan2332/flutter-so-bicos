176c51e85fdbe547f143c0a37a3575e399080e7c
Jonathan Cardoso <jonathancs2332@gmail.com>
2025-08-26 18:36:12 -0300

    improvements

---
+++ lib/ui/designsystem/themes/dimens.dart
@@ -0,0 +1,5 @@
+abstract final class Dimens {
+  const Dimens();
+
+  static const barBorderRadius = 28.0;
+}
--- lib/ui/home/widgets/home_screen.dart
+++ lib/ui/home/widgets/home_screen.dart
@@ -1,9 +1,12 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:so_bicos/routing/routes.dart';
 import 'package:so_bicos/ui/designsystem/components/app_navigation_destination.dart';
 import 'package:so_bicos/ui/designsystem/localization/app_localizations.dart';
+import 'package:so_bicos/ui/designsystem/themes/dimens.dart';
 import 'package:so_bicos/ui/home/home_view_state.dart';
 import 'package:so_bicos/ui/home/viewmodels/home_drawer_viewmodel.dart';
 import 'package:so_bicos/ui/home/viewmodels/home_viewmodel.dart';
@@ -50,12 +53,17 @@ class _HomeScreenState extends State<HomeScreen> {
         route: Routes.settings,
       ),
     ];
+    final barBorderRadius = const BorderRadius.all(Radius.circular(Dimens.barBorderRadius));
+    final barAlphaBlur = 0.7;
+    final barBlur = ImageFilter.blur(sigmaX: 3, sigmaY: 3);
     return Scaffold(
       drawer: Drawer(
         child: Builder(
           builder: (context) {
             return Padding(
-              padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),//Android 15
+              padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).padding.bottom,
+              ), //Android 15
               child: Column(
                 children: [
                   DrawerHeaderUser(viewModel: widget.drawerViewModel),
@@ -78,85 +86,101 @@ class _HomeScreenState extends State<HomeScreen> {
                 ],
               ),
             );
-          }
+          },
         ),
       ),
-      appBar: AppBar(
-        toolbarHeight: MediaQuery.of(context).padding.top + kToolbarHeight,//Android 15
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        shape: RoundedRectangleBorder(
+      extendBodyBehindAppBar: true,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(
+          MediaQuery.of(context).padding.top + kToolbarHeight,
+        ),
+        child: ClipRRect(
+          clipBehavior: Clip.antiAlias,
           borderRadius: BorderRadius.vertical(
             bottom: Radius.elliptical(MediaQuery.sizeOf(context).width, 56.0),
           ),
-        ),
-        leading: Container(),
-        flexibleSpace: Padding(
-          padding: EdgeInsets.only(
-            left: 16,
-            right: 16,
-            top: MediaQuery.viewPaddingOf(context).top,
-          ),
-          child: SearchAnchor.bar(
-            barLeading: Builder(
-              builder: (context) {
-                return IconButton(
-                  onPressed: () {
-                    Scaffold.of(context).openDrawer();
-                  },
-                  icon: Icon(Icons.menu),
-                );
-              },
+          child: BackdropFilter(
+            filter: barBlur,
+            child: AppBar(
+              backgroundColor: Theme.of(context).colorScheme.inversePrimary.withValues(alpha: barAlphaBlur),
+              leading: Container(),
+              flexibleSpace: Padding(
+                padding: EdgeInsets.only(
+                  left: 16,
+                  right: 16,
+                  top: MediaQuery.viewPaddingOf(context).top,
+                ),
+                child: SearchAnchor.bar(
+                  barLeading: Builder(
+                    builder: (context) {
+                      return IconButton(
+                        onPressed: () {
+                          Scaffold.of(context).openDrawer();
+                        },
+                        icon: Icon(Icons.menu),
+                      );
+                    },
+                  ),
+                  barHintText: appLocalizations.search,
+                  suggestionsBuilder: (context, controller) => [],
+                ),
+              ),
             ),
-            barHintText: appLocalizations.search,
-            suggestionsBuilder: (context, controller) => [],
           ),
         ),
       ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.transparent,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.all(Radius.circular(28)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withValues(alpha: 0.5),
-                    spreadRadius: 1,
-                    blurRadius: 5,
-                    offset: Offset(0, 3), // changes position of shadow
-                  ),
-                ],
-                color: Theme.of(context).colorScheme.inversePrimary,
-              ),
-              child: SizedBox(
-                height: 70,
-                width: 250,
-                child: NavigationBar(
-                  overlayColor: WidgetStateProperty.resolveWith<Color?>((
-                    states,
-                  ) {
-                    return states.contains(WidgetState.focused)
-                        ? null
-                        : Colors.transparent;
-                  }),
-                  backgroundColor: Colors.transparent,
-                  onDestinationSelected: (index) {
-                    context.go(navDestinations[index].route);
-                  },
-                  labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
-                  destinations: navDestinations,
-                  selectedIndex: navDestinations.indexWhere(
-                    (dest) =>
-                        dest.route == GoRouter.of(context).state.uri.toString(),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ClipRRect(
+            borderRadius: barBorderRadius,
+            child: BackdropFilter(
+              filter: barBlur,
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  borderRadius: barBorderRadius,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withValues(alpha: 0.5),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                  color: Theme.of(
+                    context,
+                  ).colorScheme.inversePrimary.withValues(alpha: barAlphaBlur),
+                ),
+                child: SizedBox(
+                  height: 56,
+                  width: 250,
+                  child: NavigationBar(
+                    overlayColor: WidgetStateProperty.resolveWith<Color?>((
+                      states,
+                    ) {
+                      return states.contains(WidgetState.focused)
+                          ? null
+                          : Colors.transparent;
+                    }),
+                    backgroundColor: Colors.transparent,
+                    onDestinationSelected: (index) {
+                      context.go(navDestinations[index].route);
+                    },
+                    labelBehavior:
+                        NavigationDestinationLabelBehavior.alwaysHide,
+                    destinations: navDestinations,
+                    selectedIndex: navDestinations.indexWhere(
+                      (dest) =>
+                          dest.route ==
+                          GoRouter.of(context).state.uri.toString(),
+                    ),
                   ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       body: Center(
         child: ValueListenableBuilder(
@@ -165,6 +189,13 @@ class _HomeScreenState extends State<HomeScreen> {
             if (viewState is HomeSuccessState) {
               return RefreshIndicator.adaptive(
                 child: ListView.builder(
+                  padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).padding.top,
+                    bottom:
+                        kBottomNavigationBarHeight +
+                        kFloatingActionButtonMargin +
+                        56,
+                  ),
                   itemCount: viewState.jobs.length,
                   itemBuilder: (context, index) {
                     final job = viewState.jobs[index];
